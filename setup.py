2ed3ed4790cd0bd1fb03c5aa074fe194ed350a53
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-01-24 10:15:42 -0800

    fix gpl

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
     ],
 )
